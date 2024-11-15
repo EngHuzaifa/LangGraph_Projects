02da91fb5aef325891840c1491c23be86f715416
CodeWithHuzaifa <mianhuzaifa360@gmail.com>
2024-11-15 22:06:05 +0500

    updated

--- Essay_Grading_Agent/app.py
+++ Essay_Grading_Agent/app.py
@@ -10,7 +10,7 @@ import streamlit as st
 #pip install -q -U langgraph langsmith langchain streamlit langchain_groq
 
 # Set environment variables
-os.environ["LANGCHAIN_API_KEY"] = os.getenv('LANGCHAIN_API_KEY')
+
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Build Essay Grading agent"
 
