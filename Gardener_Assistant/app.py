4552526f9d9e3e8c234e00602c1c33db299258fe
CodeWithHuzaifa <mianhuzaifa360@gmail.com>
2024-11-22 21:32:25 +0500

    Updated

--- Gardener_Assistant/app.py
+++ Gardener_Assistant/app.py
@@ -12,6 +12,12 @@ from dotenv import load_dotenv
 # Set environment variables
 load_dotenv()
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+api_key = os.getenv("TAVILY_API_KEY")
+if api_key:
+    os.environ["TAVILY_API_KEY"] = api_key
+else:
+    raise EnvironmentError("TAVILY_API_KEY is not set in the environment variables.")
+
 os.environ["LANGSMITH_TRACING_V2"] = "true"
 gemini_api_key=os.getenv('GEMINI_API_KEY')
 tavily_api_key=os.getenv('TAVILY_API_KEY')
